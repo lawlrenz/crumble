b291bb67a4d576e4240787073762f6896c7bd9ff
Lorenz Heiler <contact@lorenzheiler.com>
2015-09-25 12:42:54 +0200

    sandboxing

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import hexdump
-# import capstone
+from capstone import *
 
 filehandle = open('a.out')
 filecontent = filehandle.read()
 filehandle.close()
 
-filedump = hexdump.dump(filecontent)
-filedumpasshellcode = hexdump.restore(filedump)
-print(filedumpasshellcode)
+filedump = hexdump.dump(filecontent, size=2, sep='\\x')
+filedumpasshell = '\\x'+filedump
+print(filedumpasshell)
 
-# for i in capstone.md.disasm(filedump, 0x1000):
-#    print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+md = Cs(CS_ARCH_X86, CS_MODE_64)
+for i in md.disasm(filedumpasshell, 0x1000):
+    print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
