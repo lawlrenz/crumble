dc7b56590ac7280cb0a3725a005b6572bfa217b9
Lorenz Heiler <contact@lorenzheiler.com>
2015-09-29 14:21:07 +0200

    multithreaded, queuing, flow control..

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hexdump  # doing hexdumps obv
-import pefile  # easy information about PE file
+import hexdump  # doing hexdumps from binaries obviously
+import pefile  # easy information about PE file like entrypoint
 from capstone import *  # capstone lib for OPcodes
+import Queue  # queue module for recursive attempt
+import threading  # threading module for some performance optimizations
+import json  # json is used for saving the results of the disassembler
 
 
 def get_hexdump_from_file(filename):
@@ -10,7 +13,7 @@ def get_hexdump_from_file(filename):
 
     filedump = hexdump.dump(filecontent, size=2, sep='\\x')  # hexdump of binary
     # print(hexdump.hexdump(filecontent))
-    filedumpasshell = '\\x'+filedump
+    filedumpasshell = '\\x'+filedump  # make sure that the dump is in a readable format for capstone
     return filedumpasshell
 
 
@@ -21,24 +24,43 @@ def get_entry_point(filename):
     return baseofcode+entrypointoffset  # compute entrypoint
 
 
-def do_disassembly(hexdump_in, offset):
-    md = Cs(CS_ARCH_X86, CS_MODE_32)  # set architecture to x86, 32 bit
-    for i in md.disasm(hexdump_in, offset):
-
-        # is operation jump/function call?
-        print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))  # print linear disassembled opcodes
+def do_disassembly(offset):
+    if offset not in address_map:  # is offset allready visited?
+        address_map.append(offset)  # if not, mark it now as visited
+        md = Cs(CS_ARCH_X86, CS_MODE_32)  # set architecture to x86, 32 bit
+        pointer_on_string = address_to_string_pointer(offset)
+        tmp_hexdump = full_hexdump[pointer_on_string:pointer_on_string+64]  # todo: how long? 64bit is wrong obv
+        for i in md.disasm(tmp_hexdump, offset):
+            if i.mnemonic == 'js':  # todo: call and other jumps..
+                address_as_int = int(i.op_str, 0)
+                dsm_queue.put(address_as_int)  # is operation jump/function call? add address to queue!
+            else:
+                print("0x%x:\t%s\t%s\n" % (i.address, i.mnemonic, i.op_str))  # todo: save as json
 
 
 def address_to_string_pointer(address):
     return address*2*2  # map entrypoint to hexstring (*2 because bytewise and *2 for '\x' in string)
 
+
+def worker():
+    while True:
+        entry_point = dsm_queue.get()
+        do_disassembly(entry_point)
+        dsm_queue.task_done()
+
 if __name__ == '__main__':
     file_to_analyze = 'res/crackme2.exe'  # todo: as arg
+    num_threads = 4  # todo: as arg
 
-    hexdump = get_hexdump_from_file(file_to_analyze)  # dump file
-    entry_point = get_entry_point(file_to_analyze)  # get entry point as int
-    # print('EntryPoint: ' + str(hex(entry_point)))  # todo: aber eigentlich offset bei 530 hier..warum?
+    address_map = []  # saves allready visited control flow adresses
+    dsm_queue = Queue.Queue()  # initialize disassembly queue
+    for i in range(num_threads):
+        t = threading.Thread(target=worker)
+        t.daemon = True
+        t.start()
 
-    pointer_on_string = address_to_string_pointer(entry_point)
-    mainfunc = hexdump[pointer_on_string:pointer_on_string+46]
-    do_disassembly(mainfunc, entry_point)
+    full_hexdump = get_hexdump_from_file(file_to_analyze)  # dump file
+
+    first_entry_point = get_entry_point(file_to_analyze)  # find a starting point.. todo: not sure about that one
+    dsm_queue.put(first_entry_point)  # ..and put it in the queue
+    dsm_queue.join()  # wait for all jobs to finish
